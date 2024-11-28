6dd64d3f2fae641047e59400f7326e8fa5aeecf9
nabillahumi <nabillahumi003@gmail.com>
2024-11-28 15:27:34 +0700

    Fungsi 2 Jobsheet 12

---
+++ Percobaan.java
@@ -0,0 +1,23 @@
+public class Percobaan  {
+    
+    static int faktorialReksrusif(int n) {
+        if (n == 0) {
+            return (1);
+        } else {
+            return (n * faktorialReksrusif(n-1));
+        }
+    }
+
+    static  int faktorialIteratif(int n) {
+        int faktor = 1;
+        for (int i = n; i >= 1; i--) {
+            faktor = faktor * i;
+        }
+        return faktor;
+    }
+    public static void main(String[] args) {
+        System.out.println(faktorialReksrusif(5));
+        System.out.println(faktorialIteratif(5));
+    }
+}
+
---
+++ Percobaan3.java
@@ -0,0 +1,6 @@
+public class Percobaan3 {
+    
+    static double hitungLaba(double saldo, int tahun) {
+        if (tahun )
+    }
+}
